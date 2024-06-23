6c704113cb83e7c61a17c7694dd626ce01c0ed1e
CatTailzz <zsj1669677544@qq.com>
2024-06-24 02:04:21 +0800

    macbook pro vault backup: 2024-06-24 02:04:21

---
+++ content/posts/newfile.md
@@ -0,0 +1,5 @@
++++
+title = 'Newfile'
+date = 2024-06-24T02:01:35+08:00
+draft = false
++++
--- obs_scripts/NewBlog.js
+++ obs_scripts/NewBlog.js
@@ -3,27 +3,12 @@ const child_process = require('child_process');
 const exec = util.promisify(child_process.exec);
 
 
-function getCreateTimeAsFileName() {
-    var d = new Date();
-    var year = d.getFullYear();
-    var month = d.getMonth() + 1;
-    var day = d.getDate();
-    var hour = d.getHours();
-    var minute = d.getMinutes();
-    var second = d.getSeconds();
-    var time = year + "m" + month + "d" + day + "h" + hour + "m" + minute + "s" + second;
-    return time;
-}
-
-
-
 // execute command function
 
-async function executeCommand() {
-    const fileName = getCreateTimeAsFileName() + ".md";
+async function executeCommand(fileName) {
     const hugoPath = '/opt/homebrew/bin/hugo';
     const projectPath = '/Users/CatTail/Documents/Project/CatTail';
-    const { stdout, stderr } = await exec(`${hugoPath} new posts/${fileName}`, { cwd: projectPath });
+    const { stdout, stderr } = await exec(`${hugoPath} new posts/${fileName}.md`, { cwd: projectPath });
     console.log('stdout:', stdout);
     console.log('stderr:', stderr);
     if (stdout) {
@@ -36,5 +21,10 @@ async function executeCommand() {
 
 
 module.exports = async function (context, req) {
-    await executeCommand();
+    const fileName = await context.quickAddApi.inputPrompt("Enter file name");
+    if (fileName) {
+        await executeCommand(fileName);
+    } else {
+        new Notice("File name cannot be empty");
+    }
 }
